75abad276411ce6eafbcd97d841a7e17c95dd9b3
ezhang7423 <ezhang7423@student.palomar.edu>
2024-07-08 20:25:08 -0400

    fix weather

--- plugins/weather.sh
+++ plugins/weather.sh
@@ -6,7 +6,6 @@ sketchybar --set $NAME \
 LOCATION="42.36,-71.12"
 
 WEATHER_JSON=$(curl -s "https://wttr.in/$LOCATION?0pq&format=j1")
-
 # Fallback if empty
 if [ -z $WEATHER_JSON ]; then
   sketchybar --set $NAME label="$LOCATION"
@@ -15,6 +14,7 @@ fi
 
 TEMPERATURE=$(echo $WEATHER_JSON | jq '.current_condition[0].FeelsLikeF' | tr -d '"')
 WEATHER_DESCRIPTION=$(echo $WEATHER_JSON | jq '.current_condition[0].weatherDesc[0].value' | tr -d '"' | sed 's/\(.\{16\}\).*/\1.../')
+CITY=$(echo $WEATHER_JSON | jq '.nearest_area[0].areaName[0].value' | tr -d '"')
 
 sketchybar --set $NAME \
-  label="$TEMPERATURE$(echo '°')F • $WEATHER_DESCRIPTION"
+  label="$CITY • $TEMPERATURE$(echo '°')F • $WEATHER_DESCRIPTION"
